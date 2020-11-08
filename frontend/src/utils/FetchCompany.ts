7244586d1695690a6b3d58cf5b3f3280ed7555a8
bunatl <lukas.bunat@gmail.com>
2020-11-08 20:44:32 +0100

    fetched XML parsed into an object on server side

--- frontend/src/utils/FetchCompany.ts
+++ frontend/src/utils/FetchCompany.ts
@@ -7,7 +7,9 @@ export const FetchCompany = async (ico: string) => {
     try {
         const res = await fetch(`${process.env.REACT_APP_SERVER_URI}/api/company/${ico}`);
         const resJSON = await res.json();
-        console.log(resJSON[ "are:Ares_odpovedi" ][ "are:Odpoved" ]);
+        console.log(resJSON);
+
+        // console.log(resJSON[ "are:Ares_odpovedi" ][ "are:Odpoved" ]);
         // get only item we need
         return resJSON;
     } catch (err) {
--- server/src/api/FetchCompnay.js
+++ server/src/api/FetchCompnay.js
@@ -1,19 +1,16 @@
 const fetch = require('node-fetch');
-var parseString = require('xml2js').parseString;
+const convert = require('xml-js');
 
 const { Router } = require('express');
 const router = Router();
 
 router.get('/:ico', async (req, res, next) => {
     try {
-        const response = await fetch(`http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_or.cgi?ico=${ req.params.ico }&xml=0`);
+        const response = await fetch(`http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_or.cgi?ico=${ req.params.ico }&xml=0&ver=1.0.2`);
         const resXmlText = await response.text();
 
-        // parse string to XML
-        parseString(resXmlText, (err, result) => {
-            res.json(result);
-        });
-        // console.log(responseJSON);
+        const stringXmlObject = convert.xml2json(resXmlText, { compact: true, spaces: 4 });
+        res.json(stringXmlObject);
     } catch (error) {
         next(error);
     }
